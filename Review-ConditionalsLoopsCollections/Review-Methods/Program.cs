6f51646144cbd92e67db34ac90732a6fabe05f41
Marissa Munoz <marissamunoz@ou.edu>
2019-01-28 11:33:27 -0600

    added reversing a string

--- Review-ConditionalsLoopsCollections/Review-Methods/Program.cs
+++ Review-ConditionalsLoopsCollections/Review-Methods/Program.cs
@@ -12,27 +12,46 @@ namespace Review_Methods
         static void Main(string[] args)
         {
             //static double add = 0; class level variable
-            double valueThing =  Addition(5.5, 2);
-            Console.WriteLine(valueThing);
+            //double valueThing =  Addition(5.5, 2);
+            //Console.WriteLine(valueThing);
 
-            double val1 = 10.5;
-            double val2 = 1.5;
-            Subtraction(val1, val2);
+            //double val1 = 10.5;
+            //double val2 = 1.5;
+            //Subtraction(val1, val2);
 
+
+            //string s = "Hello World ! :) ";
+
+            //for (int i = s.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(s[i]);        //how to reverse a string!!!
+            //}
+
+            //for (int i = s.Length; i >= 0; i--)
+            //{
+            //    if(i == 0)
+            //    {
+            //        Console.WriteLine(s[i]);  //another way to reverse string!
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(s[i]);
+            //    }
+            //}
 
             Console.ReadKey();
         }
 
-        private static void Subtraction(double val1, double val2)
-        {
-            Console.WriteLine($"{val1 - val2}");
-        }
+        //private static void Subtraction(double val1, double val2)
+        //{
+        //    Console.WriteLine($"{val1 - val2}");
+        //}
 
-        static double Addition(double value1, double value2)
-        {
-            double add = value1 + value2;
-            return add; //different scope than the main method; if you were to call add in the main then you get errors, i.e. scope issue
-        }
+        //static double Addition(double value1, double value2)
+        //{
+        //    double add = value1 + value2;
+        //    return add; //different scope than the main method; if you were to call add in the main then you get errors, i.e. scope issue
+        //}
 
         
     }
