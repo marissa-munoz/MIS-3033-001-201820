7ed49bc0da97a995780fe547d36a31e2aa0922bb
Marissa Munoz <marissamunoz@ou.edu>
2019-01-30 11:42:10 -0600

    WPF thing

---
+++ Review-ConditionalsLoopsCollections/Example-WPF1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example_WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+        }
+
+        private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse ell = new Ellipse()
+            {
+                Stroke = Brushes.Aquamarine,
+                StrokeThickness = 5,
+                Fill = Brushes.Gray,
+                Height = 30,
+                Width = 30,
+            };
+
+            //ell.Stroke = Brushes.Gold;
+
+            canvas.Children.Add(ell);
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            var mouseLocation = e.MouseDevice.GetPosition(canvas);
+            Ellipse ell = new Ellipse();
+
+            if (txtColor.Text == "red")
+            {
+                ell = new Ellipse()
+                {
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 5,
+                    Fill = Brushes.Gray,
+                    Height = 30,
+                    Width = 30,
+                };
+            }
+
+            ell = new Ellipse()
+            {
+                Stroke = Brushes.Aquamarine,
+                StrokeThickness = 5,
+                Fill = Brushes.Gray,
+                Height = 30,
+                Width = 30,
+            };
+            canvas.Children.Add(ell);
+
+            Canvas.SetTop(ell, mouseLocation.Y -15);
+
+            Canvas.SetLeft(ell, mouseLocation.X -15);
+        }
+    }
+}
