f71209b8a20d71bc76a281e9c2b89f31ea697644
Marissa Munoz <marissamunoz@ou.edu>
2019-02-04 11:30:20 -0600

    Student WPF

---
+++ Review-ConditionalsLoopsCollections/WPF Student/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Student
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Student> students = new List<Student>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          //  List<Student> students = new List<Student>(); (moved up for scope to add more to the list)
+            string txtIDAsString = txtID.Text; //long way
+          
+            int studentID = Convert.ToInt16(txtIDAsString);
+
+            string fullName = txtName.Text;
+            double studentGPA = Convert.ToDouble(txtGPA.Text);
+
+            Student a = new Student();
+            a.SoonerID = Convert.ToInt16(txtIDAsString);
+            a.FirstName = txtName.Text;
+            a.GPA = Convert.ToDouble(txtGPA.Text);
+            students.Add(a);
+            var futureDate = Convert.ToDateTime("1/15/2020");
+            DateTime studentDOB = Convert.ToDateTime(txtDOB.Text);
+            var x = studentDOB.AddDays(50); //add 50 days to the date of birth
+            
+            DateTime rightNow = DateTime.Now; //get the time/date right now
+            var dif = rightNow - studentDOB; //difference between today and date of birth
+           // MessageBox.Show($"Hello, {fullName} ({studentID}) with a GPA of {studentGPA}");
+
+
+            output.Text = $"Hello {a.FirstName} ({a.SoonerID})";
+        }
+    }
+}
---
+++ Review-ConditionalsLoopsCollections/WPF Student/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Student
+{
+    class Student
+    {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance;
+
+        /// <summary>
+        /// default constructer for the Student class.
+        /// sets all the instance variables to their default values.
+        /// </summary>
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = "";
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0.0;
+            BursarBalance = 0.0;
+        }
+
+        /// <summary>
+        /// counstructor, different method signature from the default constructer for the class
+        /// overloaded constructer for the Student class that accepts the SoonerID, FirstName, LastName, and BursarBalance
+        /// </summary>
+        /// <param name="id">SoonerID</param>
+        /// <param name="fName">FirstName</param>
+        /// <param name="lName">LastName</param>
+        /// <param name="BursarBalance">BursarBalance</param>
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            //BursarBalance = BursarBalance; //scope problem
+            //fix scope problem
+            this.BursarBalance = BursarBalance;
+        }
+
+        public void MakePayment(double amount)
+        {
+            if (amount > 0 && BursarBalance > 0)
+            {
+
+                //if(BursarBalance > 0)
+                //{
+
+                //}
+                BursarBalance = BursarBalance - amount;
+            }
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+    }
+}
